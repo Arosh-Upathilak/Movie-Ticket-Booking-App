8e7be213edbe59027ed8623ed78c62276a904988
Arosh-Upathilak <aroshupathilak@gmail.com>
2026-03-30 18:17:33 +0530

    fixed the workflow issues

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
 
 const TabIndex = () => {
@@ -11,4 +11,3 @@ const TabIndex = () => {
 
 export default TabIndex
 
-const styles = StyleSheet.create({})
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -4,7 +4,9 @@ import { useState } from "react";
 
 
 export default function Index() {
+  // eslint-disable-next-line
   const [loading, setLoading] = useState<boolean>(false);
+  // eslint-disable-next-line
    const [loggedInUser, setLoggedInUser] = useState<boolean | null>(null);
   return (
     <>
